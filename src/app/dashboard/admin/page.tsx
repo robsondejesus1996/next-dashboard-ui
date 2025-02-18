bde7fce9383460ad99ac1e4a6e6f4c8962aaf4e3
Robson de Jesus <robsondejesus1996@hotmail.com>
2025-02-18 20:38:40 -0300

    feat: admin page cards componentes

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,7 +1,25 @@
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
   return (
-    <div className="">
-        AdminPage
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      <div className="w-full lg:w-2/3">
+      <div className="flex gap-4 justity-between flex-wrap">
+        <UserCard type="student"/>
+        <UserCard type="teacher"/>
+        <UserCard type="parent"/>
+        <UserCard type="saff"/>
+      </div>
+      <div className="flex gap-4 flex-col lg:flex-row">
+        <div className="w-full lg:w-1/3 h-[450px]">
+          <CountChart/>
+        </div>
+        <div className="w-full lg:w-2/3 h-[450px]"></div>
+      </div>
+      <div className=""></div>
+      </div>
+      <div className="w-full lg:w-1/3">r</div>
     </div>
   )
 }
---
+++ src/components/CountChart.tsx
@@ -0,0 +1,55 @@
+"use client";
+import Image from "next/image";
+import {
+  RadialBarChart,
+  RadialBar,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+
+const data = [
+  {
+    name: "Total",
+    count: 106,
+    fill: "white",
+  },
+  {
+    name: "Girls",
+    count: 53,
+    fill: "#FAE27C",
+  },
+  {
+    name: "Boys",
+    count: 53,
+    fill: "#C3EBFA",
+  },
+];
+
+const CountChart = () => {
+  return (
+    <div className="">
+      <div className="flex justify-between items-center">
+        <h1>Students</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20}/>
+      </div>
+      <div className="">
+      {/* <ResponsiveContainer width="100%" height="100%">
+        <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={data}>
+          <RadialBar
+            minAngle={15}
+            label={{ position: 'insideStart', fill: '#fff' }}
+            background
+            clockWise
+            dataKey="uv"
+          />
+          <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={style} />
+        </RadialBarChart>
+      </ResponsiveContainer> */}
+      </div>
+      <div className=""></div>
+    </div>
+  )
+};
+
+export default CountChart;
